48aeb86e9973120e2e5ef3af53e0041d8632fd62
Anderson Clayton Ribas <andersex03@gmail.com>
2025-05-20 10:39:32 -0300

    Add files via upload

---
+++ aula11/exe009.js
@@ -0,0 +1,6 @@
+var pais='EUA'
+if(pais=='Brasil'){
+    console.log('Brasileiro') //Condição composta
+}else{
+    console.log('Estrangeiro')
+}
---
+++ aula11/exe013.js
@@ -0,0 +1,33 @@
+var agora = new Date()
+vardiaSem = agora.getDay()
+var diaSem = 2
+
+console.log(diaSem)
+
+switch(diaSem){
+    case 0 :
+        console.log('Domingo')
+        break
+        case 1 :
+            console.log('Segunda')
+            break
+            case 2 :
+                console.log('Terça')
+                break
+                case 3 :
+                    console.log('Quarta')
+                    break
+                    case 4 :
+                        console.log('Quinta ')
+                        break
+                        case 5 :
+                            console.log('Sexta')
+                            break
+                            case 6 :
+                                console.log ('Sabado')
+                                break
+                                default:
+                                    console.log('[ERROR] Dia inválido')
+                                    break
+
+}
